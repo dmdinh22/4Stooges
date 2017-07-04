085d8b91e48020d1f973c0e46d1adb82de0b932e
dmdinh22 <dmdinh22@gmail.com>
2017-07-03 17:06:00 -0700

    added connectionstring for mssql

--- FoodMe.Web/Global.asax.cs
+++ FoodMe.Web/Global.asax.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using Nemiro.OAuth;
+using System;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -11,13 +9,26 @@ namespace FoodMe.Web
 {
     public class MvcApplication : System.Web.HttpApplication
     {
-        protected void Application_Start()
+        protected void Application_Start(object sender, EventArgs e)
         {
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            OAuthManager.RegisterClient
+              (
+                "yahoo",
+                "dj0yJmk9Qm1vZ3p2TmtQUm4zJmQ9WVdrOU4wbGlkWGxJT" +
+                "kc4bWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD0xZQ--",
+                "a55738627652db0acfe464de2d9be13963b0ba1f"
+              );
+            OAuthManager.RegisterClient
+                (
+                  "twitter",
+                  "cXzSHLUy57C4gTBgMGRDuqQtr",
+                  "3SSldiSb5H4XeEMOIIF4osPWxOy19jrveDcPHaWtHDQqgDYP9P"
+                );
         }
     }
 }
